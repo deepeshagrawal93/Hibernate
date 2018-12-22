f55bd7be6b5608363a46d81141babb9b85765221
deepeshagrawal93 <deepeshagrawal94@gmail.com>
2018-12-22 21:31:08 +0530

    hiber

---
+++ Data.java
@@ -0,0 +1,34 @@
+package mypack;
+
+public class Data {
+	private int stNo;
+	private String stName;
+	private String stAddress;
+
+	public void setStno(int stNo) {
+		this.stNo = stNo;
+	}
+
+	public int getStNo() {
+		return stNo;
+	}
+
+	public void setStName(String stName) {
+		this.stName = stName;
+	}
+
+	
+
+	public String getStName() {
+		return stName;
+	}
+
+	public void setStAddress(String stAddress) {
+		this.stAddress = stAddress;
+	}
+
+	public String getStAddress() {
+		return stAddress;
+	}
+
+}
